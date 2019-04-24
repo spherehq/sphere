6e95de0a01cd7d32736eb086c4d23968e6cabfe7
Jason Brown <jason.brown.delta@gmail.com>
2019-04-24 22:18:42 +0100

    fix: updated prisma endpoint

--- packages/database/generated/prisma-client/index.ts
+++ packages/database/generated/prisma-client/index.ts
@@ -1611,6 +1611,6 @@ export const models: Model[] = [
 export const Prisma = makePrismaClientClass<ClientConstructor<Prisma>>({
   typeDefs,
   models,
-  endpoint: `http://localhost:4466/sphere/dev`,
+  endpoint: `https://spherehq-production_jason-brown.prisma.sh/sphere/production`,
 })
 export const prisma = new Prisma()
