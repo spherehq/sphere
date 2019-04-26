5f30b2821cf8ee0fa8e4d3a65d972ea76c1c4727
Jason Brown <jason.brown.delta@gmail.com>
2019-04-26 09:52:45 +0100

    feat: added existance checks for sphere from config

--- packages/cli/src/hooks/init.ts
+++ packages/cli/src/hooks/init.ts
@@ -1,9 +1,10 @@
 import { Hook } from '@oclif/config'
 import { CLIError } from '@oclif/errors'
+import { prisma } from '@spherehq/database'
 
 import * as inquirer from 'inquirer'
 import * as path from 'path'
-import * as fs from 'fs'
+import * as fs from 'fs-extra'
 
 enum commands {
   INIT = 'init',
@@ -22,25 +23,38 @@ const hook: Hook<'init'> = async function(options) {
         )
     }
   } else {
-    switch (options.id) {
-      case commands.INIT:
-        const { decision } = await inquirer.prompt([
-          {
-            name: 'decision',
-            message:
-              'We have detected an existing instance of sphere, do you wish to continue?',
-            type: 'list',
-            default: false,
-            choices: [
-              { name: 'yes', value: true },
-              { name: 'no', value: false },
-            ],
-          },
-        ])
+    // confirm config alias exists
+    const { alias } = fs.readJSONSync(
+      path.join(this.config.configDir, `config.json`),
+    )
+    try {
+      const exists = await prisma.$exists.sphere({ alias })
 
-        if (!decision) {
-          this.exit()
-        }
+      switch (options.id) {
+        case commands.INIT:
+          // if it exists warn we may override existing config
+          if (exists) {
+            const { decision } = await inquirer.prompt([
+              {
+                name: 'decision',
+                message:
+                  'We have detected an existing instance of sphere, do you wish to continue?',
+                type: 'list',
+                default: false,
+                choices: [
+                  { name: 'yes', value: true },
+                  { name: 'no', value: false },
+                ],
+              },
+            ])
+
+            if (!decision) {
+              this.exit()
+            }
+          }
+      }
+    } catch (error) {
+      this.error(error)
     }
   }
 }
