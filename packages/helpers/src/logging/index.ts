d829e25277e129a032ede78bf64722a8b3e6c9b7
Jason Brown <jason.brown.delta@gmail.com>
2019-12-11 19:41:02 +0000

    feat: updated build and export

--- packages/helpers/src/logging/index.ts
+++ packages/helpers/src/logging/index.ts
@@ -2,6 +2,8 @@ import * as winston from 'winston'
 import { Timber } from '@timberio/node'
 import { TimberTransport } from '@timberio/winston'
 
+export type Logger = winston.Logger
+
 export const logger = (apiKey: string, sourceKey: string) => {
   const timber = new Timber(apiKey, sourceKey, { ignoreExceptions: false })
   return winston.createLogger({
